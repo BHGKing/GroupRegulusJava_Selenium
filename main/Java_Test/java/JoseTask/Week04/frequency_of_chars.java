575c162b74a870bab62a74dbf7c91f832e6743a8
BHGKing <josecantu111@gmail.com>
2023-06-12 15:29:50 -0500

    updated week04 to include task

--- main/Java_Test/java/JoseTask/Week04/frequency_of_chars.java
+++ main/Java_Test/java/JoseTask/Week04/frequency_of_chars.java
@@ -1,4 +1,8 @@
-package JoseTask.Java.Week04;
+package JoseTask.Week04;
 
 public class frequency_of_chars {
 }
+/*
+Write a return method that can find the frequency of characters
+  Ex: frequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
