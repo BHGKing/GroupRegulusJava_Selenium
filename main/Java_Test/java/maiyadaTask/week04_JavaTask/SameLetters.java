2ee22ac189077a7dd51d1715ecaaca22da6fa3d1
maiyada <maiyada.hasan12@gmail.com>
2023-06-15 14:18:54 -0500

    Added week04

--- main/Java_Test/java/maiyadaTask/week04_JavaTask/SameLetters.java
+++ main/Java_Test/java/maiyadaTask/week04_JavaTask/SameLetters.java
@@ -6,17 +6,7 @@ public class SameLetters {
     //  Ex: same("abc", "cab"); ==> true
     //      same("abc", "abb"); ==> false
 
-    public static boolean sameLetter(String str1, String str2){
 
 
-        if(str1.equals(str2)){
-            return true;
-        }else {
-            return false;
-        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(sameLetter("abc", "cab"));
-    }
-}
