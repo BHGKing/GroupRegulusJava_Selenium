1bb286303384441cc030fdb834617866ef6913ed
BHGKing <josecantu111@gmail.com>
2023-06-15 13:05:19 -0500

    updated removeDuplicates task to include correct prompt

--- main/Java_Test/java/JoseTask/Week04/removeDuplicates.java
+++ main/Java_Test/java/JoseTask/Week04/removeDuplicates.java
@@ -3,6 +3,6 @@ package JoseTask.Week04;
 public class removeDuplicates {
 }
 /*
-Write a return method that can find the frequency of characters
-  Ex: frequencyOfChars("AAABBCDD") ==> A3B2C1D2
+Write a return method that can remove the duplicated values from the String
+  Ex: removeDup("AAABBBCCC") ==> ABC
  */
